28845292f889fc02e2e73258550368221dd7f1a8
Aayan <aayansingh806@gmail.com>
2023-01-15 18:46:47 +0530

    first commit

---
+++ OOD/src/ood_intro/third.java
@@ -0,0 +1,66 @@
+package ood_intro;
+
+public class third {
+	public static void main(String[] args) {
+		
+		Calculator calc1 = new Calculator();
+		calc1.add(6);
+		calc1.add(6,5);
+		calc1.add(6,5,4);
+		
+		Animal animal = new Animal();
+		Dog dog = new Dog();
+		Cat cat = new Cat();
+		
+		animal.speak();
+		dog.speak();
+		cat.speak();
+	}
+}
+
+
+class Calculator{
+	
+	int a;
+	int b;
+	int c;
+	int res;
+	
+	
+	// Function Overloading (Compile Time Polymorphism)
+	void add(int a){
+		res = a + 1;
+		System.out.println(res);
+	}
+	void add(int a, int b, int c){
+		res = a + b + c;
+		System.out.println(res);
+	}
+	void add(int a, int b){
+		res = a + b;
+		System.out.println(res);
+	}
+}
+
+class Animal{
+	
+	void speak(){
+		System.out.println("Haayee!!");
+	}
+}
+
+class Dog extends Animal{
+	 
+	void speak(){
+		System.out.println("Woff!!");
+	}
+}
+
+class Cat extends Animal{
+	 
+	void speak(){
+		System.out.println("Meoww!!");
+	}
+}
+
+
